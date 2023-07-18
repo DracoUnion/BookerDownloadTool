cffcd7948e404e7ca886f9b3b8f7e53353c3a737
wizardforcel <wizard.z@qq.com>
2023-07-18 09:55:50 +0800

    2023-07-18 09:55:50

--- BookerDownloadTool/util.py
+++ BookerDownloadTool/util.py
@@ -131,3 +131,12 @@ def create_driver():
     })
     return driver
 
+def dict_get_recur(obj, keys):
+    res = [obj]
+    for k in keys.split('.'):
+        k = k.strip()
+        if k == '*':
+            res = reduce(lambda x, y: x + y,res, [])
+        else:
+            res = [o.get(k) for o in res if k in o]
+    return res
