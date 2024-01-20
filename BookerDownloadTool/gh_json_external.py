88a2f4ebb166b2291e88f4fa244bfbde19fe343a
wizardforcel <562826179@qq.com>
2024-01-20 20:50:48 +0800

    2024-01-20 20:50:48

--- BookerDownloadTool/gh_json_external.py
+++ BookerDownloadTool/gh_json_external.py
@@ -1,6 +1,36 @@
 from EpubCrawler.config import config
 from pyquery import PyQuery as pq
 import json
+import re
+from urllib.parse import urljoin
+
+def get_toc(jstr, base):
+    j = json.loads(jstr)
+    html = j['payload']['blob']['richText']
+    rt = pq(html)
+    if config['remove']:
+        rt.remove(config['remove'])
+    el_links = rt(config['link'])
+    vis = set()
+    res = []
+    for i in range(len(el_links)):
+        el_link = el_links.eq(i)
+        url = el_link.attr('href')
+        if not url:
+            text = el_link.text().strip()
+            res.append(text)
+            continue
+            
+        url = re.sub(r'#.*$', '', url)
+        if base:
+            url = urljoin(base, url)
+        if not url.startswith('http'):
+            continue
+        if url in vis: continue
+        vis.add(url)
+        res.append(url)
+        
+    return res
 
 def get_article(jstr, base):
     j = json.loads(jstr)
