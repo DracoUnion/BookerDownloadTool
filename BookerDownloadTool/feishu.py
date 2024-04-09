681839c201ca04faa4dccc842913993765d88ad5
wizardforcel <wizard.z@qq.com>
2024-04-09 17:25:55 +0800

    2024-04-09 17:25:55

--- BookerDownloadTool/feishu.py
+++ BookerDownloadTool/feishu.py
@@ -42,7 +42,7 @@ def blk2html(blk):
     else:
         raise ValueError()
 
-def get_docx_html(uid, aid):
+def get_docx_html(uid, aid, cookie=''):
     url = f'https://{uid}.feishu.cn/space/api/docx/pages/client_vars?id={aid}&limit=100000'
     hdrs = default_hdrs | {'Cookie': cookie}
     data = request_retry('GET', url, headers=hdrs).json()
