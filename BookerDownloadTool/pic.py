c1102c852f867920e4b9ed9cf86ca80862b183fb
wizardforcel <wizard.z@qq.com>
2024-07-08 17:48:23 +0800

    2024-07-08 17:48:21

--- BookerDownloadTool/pic.py
+++ BookerDownloadTool/pic.py
@@ -31,6 +31,10 @@ def download_pixabay(args):
         print(f'page: {i}')
         url = f'https://pixabay.com/images/search/{kw}/?pagi={i}'
         driver.get(url)
+        driver.execute_script('''
+            document.body.style.zoom = '1%';
+            window.scrollTo(0, document.body.scrollHeight);
+        ''')
         driver.implicitly_wait(5)
         '''
         html = request_retry(
@@ -43,8 +47,12 @@ def download_pixabay(args):
         el_pics = pq(html).find('div[class^=container]>a[class^=link]')
         for el in el_pics:
             el = pq(el)
-            img_url = el.children('img').attr('srcset').split(',\x20')[-1].split('\x20')[0]
-            img_fname = path.basename(el.attr('alt')[:-1]) + '.png'
+            el_img = el.children('img').eq(0)
+            if el_img.attr('srcset'):
+                img_url = el_img.attr('srcset').split(',\x20')[-1].split('\x20')[0]
+            else:
+                img_url = el_img.attr('src')
+            img_fname = path.basename(el.attr('href')[:-1]) + '.png'
             h = pool.submit(tr_download_pic_safe, img_url, img_fname, args)
             hdls.append(h)
             if len(hdls) >= args.threads:
