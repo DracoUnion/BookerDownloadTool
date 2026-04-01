dd9a6e59faec0eb96071f057a8a9a072a0d05ccd
wizardforcel <562826179@qq.com>
2026-04-01 14:39:01 +0800

    2026-04-01 14:39:01

--- BookerDownloadTool/process_img_md.py
+++ BookerDownloadTool/process_img_md.py
@@ -3,6 +3,7 @@ import hashlib
 import requests
 from os import path
 import traceback
+from imgyaso.quant import pngquant
 from urllib.parse import urljoin
 
 def process_img_md(md, imgs, base_url='', img_prefix='img/'):
@@ -17,6 +18,7 @@ def process_img_md(md, imgs, base_url='', img_prefix='img/'):
             print(url)
             hash_ = hashlib.md5(url.encode('utf8')).hexdigest()
             data = requests.get(url).content
+            data = pngquant(data)
             imgs[f'{hash_}.png'] = data
             md = md.replace(url, f'{img_prefix}{hash_}.png')
         except:
