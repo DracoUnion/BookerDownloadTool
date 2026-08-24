80e8e8c15d009c3b39bfdecee2c01e4e6bf01dce
wizardforcel <562826179@qq.com>
2026-08-25 00:01:44 +0800

    2026-08-25 00:01:44

--- BookerDownloadTool/__main__.py
+++ BookerDownloadTool/__main__.py
@@ -28,7 +28,7 @@ def main():
 
     parser = argparse.ArgumentParser(prog="BookerDownloadTool", formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("-v", "--version", action="version", version=f"PYBP version: {__version__}")
-    parser.add_argument("-H", "--headless", action="store_true")
+    parser.add_argument("-H", "--headless", action="store_false")
     parser.set_defaults(func=lambda x: parser.print_help())
     subparsers = parser.add_subparsers()
     
