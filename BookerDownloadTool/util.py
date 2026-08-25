9c1a5e00cfa06aab6329313ca5e23a40c36c860d
wizardforcel <562826179@qq.com>
2026-08-25 17:01:36 +0800

    2026-08-25 17:01:36

--- BookerDownloadTool/util.py
+++ BookerDownloadTool/util.py
@@ -146,7 +146,7 @@ def camou_create_driver(headless=True, timeout=30_000):
     with Camoufox(**opts) as browser:
         context = browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            viewport={"width":1920,"height":1080},
+            viewport={"width":1200,"height":640},
             locale="zh-CN",
             timezone_id="Asia/Shanghai"
         )
